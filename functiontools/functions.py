253bec5d86644cd98f64b0b96f14a74ab5c9c830
Nina Dickens <ninadickens@berkeley.edu>
2022-05-11 07:43:45 +0000

    adding functiontools tests

--- functiontools/functions.py
+++ functiontools/functions.py
@@ -8,20 +8,39 @@ from scipy.io import wavfile
 #Data Cleanup Function 
 
 def cleanup(df, years):
-    # replace + and - and * so they are coded as missing
-    df = df.replace('+', np.NaN, regex=False)
-    df = df.replace('-', np.NaN, regex=False)
-    df = df.replace('*', np.NaN, regex=False)
-    
-    # remove $ sign
-    df[years] = df[years].replace('[\$,]', '', regex=True).astype(float)
+	# replace + and - and * so they are coded as missing
+	df = df.replace('+', np.NaN, regex=False)
+	df = df.replace('-', np.NaN, regex=False)
+	df = df.replace('*', np.NaN, regex=False)
+	
+	# remove $ sign
+	df[years] = df[years].replace('[\$,]', '', regex=True).astype(float)
 
-    # remove , in mortality
-    df['2019_US_Mortality_19'] = df['2019_US_Mortality_19'].replace('[,,]', '', regex=True).astype(float)
-    pd.to_numeric(df['2019_US_Mortality_19'])
+	# remove , in mortality
+	df['2019_US_Mortality_19'] = df['2019_US_Mortality_19'].replace('[,,]', '', regex=True).astype(float)
+	pd.to_numeric(df['2019_US_Mortality_19'])
+	
+	return df
+
+def draw_barchart(df, year):
+	
+	datayear = df[["research_category", year]]
     
-    return df
+	# Based on the year it will output a top 10 spending categories for that year
+	datayear[year] = datayear[year].replace('[\$,]', '', regex=True).astype(float)
+	datayeartop10 = datayear.sort_values(year, ascending=False)[:10]
+    
+	fig, ax = plt.subplots(figsize=(15, 8))
+    
+	# Graphs a top 10 for the categories in that year
+	ax.barh(datayeartop10["research_category"],datayeartop10[year])
+	ax.set_title("Top 10 Spending Categories")
+	ax.set_xlabel("Spending ($)")
+	ax.set_ylabel("Spending Category")
+	ax.grid(which='major', axis='x', linestyle='-')
+	plt.savefig('figures/fig1b.png');
+
 
 def change_to_float(data):
-    return data.replace('[\$,]', '', regex=True).astype(float)
+	return data.replace('[\$,]', '', regex=True).astype(float)
 
--- functiontools/test/test_functions.py
+++ functiontools/test/test_functions.py
@@ -2,6 +2,8 @@
 #Testing cleanup function 
 from functiontools import functions as functions
 import numpy
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def test_cleanup():
 	df = pd.read_csv (r'data/raw_data/funding_data.csv')
@@ -12,12 +14,20 @@ def test_cleanup():
 	#Testing to see "$" was deleted
 	assert df["2011"].values[2] == 69
 	
+
+# Testing draw_barchart function
+def test_draw_barchart():
+    img = plt.imread('figures/fig1b.png')
+    assert img is not None
 	
-def test_to_float():
-	df = pd.read_csv (r'data/raw_data/funding_data.csv')
+
+#Third Test Function to test float_change function
+
+#def test_to_float():
+	#df = pd.read_csv (r'data/raw_data/funding_data.csv')
+	#data2019 = df[["research_category", "2019"]]
 	
-	df = functions.change_to_float(df)
+	#dff = functions.change_to_float(data2019["2019"])
 	
-	assert type(df["2011"].values[2]) == numpy.float64
-	
-	
+	#assert type(dff["2019"].values[2]) == numpy.float64
+   
